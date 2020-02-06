ab6647efbe7c5a0722f1e6b916187bc36bc3a540
ivan-drr <ivan.delrr@gmail.com>
2020-02-06 08:48:39 +0100

    structure login component

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+
+    render() {
+        return (
+            <div id="loginComponent">
+                <h2>Codigo HTML aqui</h2>
+                <i>Esto es JSX, basicamente HTML dentro de js</i>
+            </div>
+        );
+    }
+
+}
+
+export default Login;
