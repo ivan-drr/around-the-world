5dd308db519e773a4dc84a4ab506c61284777f48
ivan-drr <ivan.delrr@gmail.com>
2020-02-12 10:05:00 +0100

    Login modal

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,46 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import $ from 'jquery';
 import { Link } from 'react-router-dom';
 
-import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 import '../styles/Login.css';
 
 function Login() {
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div id="loginComponent">
-      <Card className="text-center border-shadow">
-        <Card.Header>Access to your account</Card.Header>
-        <Card.Body>
+      <Button variant="primary" onClick={handleShow}>
+        Login
+      </Button>
+      <Modal show={show} onHide={handleClose} className="text-center border-shadow">
+        <Modal.Header closeButton>
+          <Modal.Title>Accede a tu cuenta</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Correo Electronico</Form.Label>
+              <Form.Control type="email" placeholder="Introduce tu correo" />
+              <Form.Text className="text-muted">
+                Nunca compartiremos tu correo con nadie.
+              </Form.Text>
+            </Form.Group>
 
-            <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control type="password" placeholder="Contraseña" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Recuerdame" />
+            </Form.Group>
+          </Form>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Remember me" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Login
-              </Button>
-            </Form>
-
-        </Card.Body>
-        <Card.Footer className="text-muted">
+          <Button variant="primary" onClick={handleClose} className="mr-2">
+            Guardar Cambios
+          </Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+        </Modal.Body>
+        <Modal.Footer>
           <Link to="signup">New here? Create an account</Link>
-        </Card.Footer>
-      </Card>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
