3141ad0df39bd93cc321631462c48a1fde358f32
ivan-drr <ivan.delrr@gmail.com>
2020-02-18 20:06:54 +0100

    navbar build advice

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,16 @@ import Button from 'react-bootstrap/Button';
 import '../styles/ButtonsComponent.css';
 
 function Navbar() {
+    // estas constantes definen un estado "show" del componente
+    // creamos dos setters de la variable estado "handleClose"
+    // y "handleShow", hacemos esto para mostrar o no por ejemplo un Modal
+    // ya que el componente modal de react bootstrap tiene una propiedad
+    // llamada "show" que lo muestra si su valor es true y lo esconde si es false
+    // en este caso no lo estas usando, pero seria buena idea utilizarlas para
+    // crear un modal para añadir viajes como puede ser el modal del componente login.
+    // PERO si utilizamos dos modales: añadir viajes y ver viajes, entonces no podremos
+    // utilizar la misma variable ya que se mostrarian los dos al pulsar cualquiera de
+    // los dos botones.
 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
@@ -16,7 +26,7 @@ function Navbar() {
                 <Button variant="outline-secondary" onClick={handleShow}>Añadir</Button>
             </div>
             <div id="buttonComponent2">
-                <Button variant="outline-secondary" onClick={handleShow}>Ver viajes</Button>
+                <Button variant="outline-secondary">Ver viajes</Button>
             </div>
         </div>
     );
